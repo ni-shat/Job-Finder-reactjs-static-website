09129d9e4ede09d2953cb18df8a4a2aa70e33dc2
ni-shat <nishat.tabassum301@gmail.com>
2023-04-11 17:07:41 +0600

    actions of see-all-btn are defined

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
-const FeaturedJob = ({ job }) => {
+const FeaturedJob = ({ job}) => {
 
     const { id, companyName, contactInfo, requirements, fullOrPartTime, Experiences, jobTitle, jobDescription, jobResponsibility, jobType, location, logo, salary } = job;
 
-    console.log(id, companyName, contactInfo, requirements, fullOrPartTime, Experiences, jobTitle, jobDescription, jobResponsibility, jobType, location, logo, salary);
+    // const [jobRepresentingState, setJobRepresentingState] = useState(false);
+    // const [_id, setId] = useState(0);
 
-    console.log("BREAK");
+    // useEffect(() => {
+
+    //     if (id > 4 && seeAllState == true) {
+    //         // setId(parseInt(id));
+    //         setJobRepresentingState(true);
+    //     }
+
+    //     else if (id > 4 && seeAllState == false) {
+    //         setJobRepresentingState(false);
+    //     }
+    //     else if(id < 5){
+    //         setJobRepresentingState(true);
+    //     }
+
+    // }, [])
+
+
     return (
+
         <div className='border rounded-lg p-9 space-y-4'>
+
             {/* <div className='h-[100px] w-36 bg-slate-200 mb-8'>
                 <img className='h-full object-scale-down' src={logo} alt="" />
             </div> */}
@@ -21,13 +40,14 @@ const FeaturedJob = ({ job }) => {
             <h3 className='text-2xl font-bold text-[#474747]'>{jobTitle}</h3>
             <p className='text-[#7d7d7d] font-semibold text-base'>{companyName}</p>
             <div className='flex gap-8'>
-                <button className='border border-[#9873FF] text-[#9873FF] p-2 px-6 rounded text-xl font-semibold'>{jobType}</button>
-                <button className='border border-[#9873FF] text-[#9873FF] p-2 px-6 rounded text-xl font-semibold'>{fullOrPartTime}</button>
+                <button className='border border-[#9873FF] text-[#9873FF] p-4 px-8 rounded text-xl font-semibold'>{jobType}</button>
+                <button className='border border-[#9873FF] text-[#9873FF] p-4 px-8 rounded text-xl font-semibold'>{fullOrPartTime}</button>
             </div>
-            <div className='flex text-[#7d7d7d] gap-7 font-semibold'>
-                <div><FontAwesomeIcon className='mr-4' icon={faLocationDot} />{location}</div>
+            <div className='flex text-[#7d7d7d] gap-8 font-semibold'>
+                <div><FontAwesomeIcon className='mr-2' icon={faLocationDot} />{location}</div>
                 <div>
-                    <div className='inline-block rounded-full border-2 text-center w-7 h-7 mr-2'><FontAwesomeIcon className='text-center' icon={faDollarSign} /></div>Salary: {salary}
+                    <div className='inline-flex justify-center items-center rounded-full border-2 w-5 h-5 mr-2'><FontAwesomeIcon className='text-xs' icon={faDollarSign} />
+                    </div>Salary: {salary}
                 </div>
             </div>
             <button className='bg-gradient-to-r from-[#7E90FE] from-0% to-[#9873FF] to-100%  p-3 px-8 rounded-md text-xl text-white font-semibold'>View Details</button>
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -6,13 +6,29 @@ import FeaturedJob from '../FeaturedJob/FeaturedJob';
 const FeaturedJobs = () => {
 
     const [featuredJobs, useFeaturedJobs] = useState([]);
+    const [seeAllState, setSeeAllState] = useState(false);
+    const [hideBtn, setHideBtn] = useState(false);
+
 
     const getFeaturedJobs = useLoaderData();
-    console.log(getFeaturedJobs);
+    // console.log(getFeaturedJobs);
 
     useEffect(() => {
-        useFeaturedJobs(getFeaturedJobs);
-    }, [])
+        if(seeAllState == false){
+            const filteredJobs = getFeaturedJobs.filter(job => (parseInt(job.id) < 5));
+            console.log("FILtered jobs :", filteredJobs);
+            useFeaturedJobs(filteredJobs);
+        }
+        else{        
+            useFeaturedJobs(getFeaturedJobs);
+        }
+
+    }, [seeAllState]);
+
+    const getSeeAllState = () => {
+        setSeeAllState(true);
+        setHideBtn(true);
+    }
 
     return (
         <div className='w-[65%] mx-auto mb-32'>
@@ -25,6 +41,9 @@ const FeaturedJobs = () => {
                         job={job}
                     ></FeaturedJob>)
                 }
+            </div>
+            <div className='text-center mt-10'>
+                <button onClick={getSeeAllState} className={`bg-gradient-to-r from-[#7E90FE] from-0% to-[#9873FF] to-100%  p-4 px-8 rounded-md text-xl text-white font-semibold ${hideBtn && 'hidden'}`}>See All Jobs</button>
             </div>
 
         </div>

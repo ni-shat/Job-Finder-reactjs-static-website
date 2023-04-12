83d3eaa7564c3870f208fed3fe6f0536cbda20c7
ni-shat <nishat.tabassum301@gmail.com>
2023-04-12 23:47:53 +0600

    completed the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
     return (
         <div className='bg-[#F9FAFF] pt-20 md:pt-0'>
             <div className='flex flex-col md:flex-row gap-10 md:gap-0 justify-between  w-[95%] lg:w-[65%] mx-auto'>
-                <div className='md:mr-8 -mr-0 mx-auto text-center md:text-start md:w-2/4 flex flex-col gap-4 md:gap-10 justify-center'>
-                    <h1 className='lg:text-7xl text-center md:text-start  text-3xl font-bold w-[85%] mx-auto lg:leading-[75px] mb-4 md:mb-0'>One Step Closer To Your <span className='text-[#7E90FE]'>Dream Job</span></h1>
+                <div className='md:mr-8 -mr-0  text-center md:text-start md:w-2/4 flex flex-col gap-4 md:gap-10 justify-center'>
+                    <h1 className='lg:text-5xl text-center md:text-start  text-3xl font-bold w-[85%] mx-auto md:mx-0 lg:leading-[50px] mb-4 md:mb-0'>One Step Closer To Your <span className='text-[#7E90FE]'>Dream Job</span></h1>
                     <p className='w-[95%] md:mt-0 -mt-4 md:text-xl text-base md:leading-8 text-[#474747]'>
                         Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
                     </p>

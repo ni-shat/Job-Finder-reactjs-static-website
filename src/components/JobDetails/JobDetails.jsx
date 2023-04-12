4efa0fb90f0526c63fc5f7c154236db3513c5efa
ni-shat <nishat.tabassum301@gmail.com>
2023-04-12 07:19:02 +0600

    job details section is implemented for all job id

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -33,6 +33,7 @@ const JobDetails = () => {
 
 
     // console.log(jobDescription, requirements, Experiences, jobResponsibility);
+    console.log(job)
 
 
 
@@ -51,40 +52,78 @@ const JobDetails = () => {
 
             </div>
 
-            <div className='mt-32 flex w-[65%] mx-auto bg-slate-200'>
+            <div className='mt-32 flex gap-6 w-[65%] mx-auto'>
                 <div className='w-[70%]'>
                     {
-                        job && <div className='text-[#474747] text-xl mb-12'><span className='font-semibold'>Job description:</span> {job.jobDescription}</div>
+                        job.jobDescription && <div className='text-[#474747] text-xl mb-12'><span className='font-bold text-2xl'>Job description:  </span> {job.jobDescription}</div>
                     }
 
                     {
                         jobResponsibility &&
-                        <div className='text-[#474747] text-xl mb-12'><span className='font-semibold'>Job Responsibility:</span>
+                        <div className='text-[#474747] text-xl mb-12'><span className='font-bold text-2xl'>Job Responsibility:</span>
                             {
                                 <UL array={jobResponsibility}></UL>
                             }
                         </div>
                     }
                     {
-                        requirements && 
-                        <div className='text-[#474747] text-xl mb-12'><span className='font-semibold'>Educational Requirements:</span>
+                        requirements &&
+                        <div className='text-[#474747] text-xl mb-12'><span className='font-bold text-2xl'>Educational Requirements:</span>
                             {
                                 <UL array={requirements}></UL>
                             }
                         </div>
                     }
                     {
-                        job.Experiences && 
-                        <div className='text-[#474747] text-xl mb-12'><span className='font-semibold'>Experiences:</span> {job.Experiences}</div>
+                        job.Experiences &&
+                        <div className='text-[#474747] text-xl mb-12'><span className='font-bold text-2xl'>Experiences:</span> {job.Experiences}</div>
                     }
 
                 </div>
-                <div className="w-[30%]">
-                    <div>
-                        
+
+                {
+                    job &&
+
+                    <div className='w-[30%] '>
+
+                        <div className="p-8 bg-[#F9F7FF] rounded-md">
+                            <div>
+                                <h2 className='text-2xl font-bold mb-4'>Job details</h2>
+                                <hr className='mb-6' />
+                                <p className='text-[#474747] text-xl mb-1'>
+                                    <span className='font-semibold'>Salary:</span> {job.salary}
+                                </p>
+                                <p className='text-[#474747] text-xl mb-12'>
+                                    <span className='font-semibold'>Job title:</span> {job.jobTitle}
+                                </p>
+                            </div>
+
+                            <div>
+                                <h2 className='text-2xl font-bold mb-4'>Contact Information</h2>
+                                <hr className='mb-6' />
+                                {
+                                    job?.contactInfo?.Phone &&
+                                    <p className='text-[#474747] text-xl mb-1'>
+                                        <span className='font-semibold'>Phone:</span> {job.contactInfo.Phone}
+                                    </p>
+                                }
+                                {
+                                    job?.contactInfo?.email &&
+                                    <p className='text-[#474747] text-xl mb-1'>
+                                        <span className='font-semibold'>Email:</span>{job.contactInfo.email}
+                                    </p>
+                                }
+                                {
+                                    job?.contactInfo?.location &&
+                                    <p className='text-[#474747] text-xl'>
+                                        <span className='font-semibold'>Address:</span>{job.contactInfo.location}
+                                    </p>
+                                }
+                            </div>
+                        </div>
+                        <button className='bg-gradient-to-r from-[#7E90FE] from-0% to-[#9873FF] to-100%  p-4 px-8 rounded-md text-xl text-white font-semibold mt-8 w-full'>Apply Now</button>
                     </div>
-                    <div></div>
-                </div>
+                }
             </div>
         </div>
     );
